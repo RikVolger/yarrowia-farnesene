f429c443222abbac30129c5b31afccd43cdec36c
RikVolger <rvolger@gmail.com>
2021-12-08 14:39:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# The main file orchestrating the execution of the various parts of the model
+# importing used packages
+import cobra
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
